3f36f8a25e39fb07df8d822af424023946f7cc96
мусатова.анастасия <.@TERM1>
2025-01-27 12:01:57 +0300

    efr

--- View/Report2Page.xaml.cs
+++ View/Report2Page.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Control8.AppData;
+using Control8.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,26 @@ namespace Control8.View
         public Report2Page()
         {
             InitializeComponent();
+            SpecialCmb.SelectedValuePath = "Id";
+            SpecialCmb.DisplayMemberPath = "Name";
+            SpecialCmb.ItemsSource = App.context.Spesial.ToList();
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int SelectedSpecial = Convert.ToInt32(SpecialCmb.SelectedValue);
+
+            DataGridInfo.ItemsSource = App.context.Group.Where(x => x.IdSpesial == SelectedSpecial).ToList();
+        }
+
+        private void DataGridInfo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void MarkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.FrameBody.Navigate(new View.Report2_1Page((sender as Button).DataContext as Group));
         }
     }
 }
---
+++ View/Report2_1Page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Control8.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control8.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Report2_1Page.xaml
+    /// </summary>
+    public partial class Report2_1Page : System.Windows.Controls.Page
+    {
+        public Report2_1Page(Group group)
+        {
+            InitializeComponent();
+            GridJournal.ItemsSource = App.context.Journal.Where(x => x.IdGroup == group.Id).ToList();
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog =   new PrintDialog();
+            if (printDialog.ShowDialog() == true) printDialog.PrintVisual(GridJournal, "Баллы");
+        }
+    }
+}
